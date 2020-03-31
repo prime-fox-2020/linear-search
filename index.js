fdcd1eb307026712223ce5e604aac03dd837489e
syukur <syukurbaru@gmail.com>
2020-03-31 15:10:44 +0700

    add file linear search js.

--- index.js
+++ index.js
@@ -4,9 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target)
+      return i
+  }
+  return -1
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -17,6 +22,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var tamp = []
+  for (var j = 0; j < values.length; j++) {
+    if (values[j] === target) {
+      tamp.push(j)
+    }
+  }
+  return tamp
 }
 
 let banana_arr = "banana".split("");
