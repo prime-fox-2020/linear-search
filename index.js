576a918f6767ad47b72c5c8b4abfb01485c49ede
riolly <albert.r.cahyantara@gmail.com>
2020-03-31 16:09:43 +0700

    Release 0: Basic Linear Search

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -14,19 +19,19 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
-console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
+// console.log("Global Linear Search")
+// let globalLinearSearch = (target, values) => {
   //write your code here
-}
+// }
 
-let banana_arr = "banana".split("");
+// let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
+// console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
 
 // abaikan code dibawah
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
