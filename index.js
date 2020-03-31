8f3bf25b4531e7078fd0c8e9ee1cb493119446f5
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-03-31 11:07:53 +0700

    done

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (let i = 0; i < values.length; i++) {
+    if(values[i] === target) return i;
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +19,11 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let res = [];
+  for (let i = 0; i < values.length; i++) {
+    if(values[i] === target) res.push(i);
+  }
+  return res;
 }
 
 let banana_arr = "banana".split("");
