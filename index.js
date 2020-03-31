dc8bf3b37d39ffe71f442b34d97dc8239985f930
susilo hartomo <encusilo545110@gmail.com>
2020-03-31 14:30:49 +0700

    release 0

--- index.js
+++ index.js
@@ -4,6 +4,16 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var check = true
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] == target) {
+      check = false
+      return i
+    }
+  }
+  if (check) {
+    return -1
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
