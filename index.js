e7cd43d6678bdc36f028bc15fad2dfd579a61e01
Sujackson <su_jackson99@yahoo.com>
2020-03-31 21:16:03 +0700

    done

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let i = 0; i < values.length; i++) {
+    if(values[i]==target)
+    {
+      return i
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +24,16 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var res=[]
+  for (let i = 0; i < values.length; i++) {
+    if(target==values[i]){
+      res.push(i)
+    }
+  }
+  if(res.length==0){
+    return -1
+  }
+  return res
 }
 
 let banana_arr = "banana".split("");
