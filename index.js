713050c125123588af3140e1c747c82bf18cba6e
akmaliavitri <akmaliavitri21@gmail.com>
2020-03-31 14:01:54 +0700

    linear-search

--- index.js
+++ index.js
@@ -4,6 +4,15 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let result = -1
+  for(let i=0; i<values.length; i++){
+    if(target === values[i]){
+      result = i
+      break
+    }
+  }
+
+  return result
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +26,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let result = []
+  for(let i=0; i<values.length; i++){
+    if(target === values[i]){
+      result.push(i)
+    }
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
