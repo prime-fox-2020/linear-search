70fce1d0d86a8020456d9bf430ed37df7cc4b57c
elvanrafif <terlanjurbuta@gmail.com>
2020-03-31 19:56:39 +0700

    add tugas linear search

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-'use strict'
+"use strict";
 
 // Release 0
-console.log("Linear Search")
+console.log("Linear Search");
 let linearSearch = (target, values) => {
-  //write your code here
-}
+  var index = 0;
+  for (var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      index += 1;
+    }
+  }
+  if (index == 0) {
+    return -1;
+  }
+  return index;
+};
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -14,19 +23,28 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
-console.log("Global Linear Search")
+console.log("Global Linear Search");
 let globalLinearSearch = (target, values) => {
-  //write your code here
-}
+  var res = [];
+  for (var i = 0; i < banana_arr.length; i++) {
+    if (target == banana_arr[i]) {
+      res.push(i);
+    }
+  }
+  if (res.length !== 0) {
+    return res;
+  } else {
+    return "Character not found";
+  }
+};
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-
 // abaikan code dibawah
 module.exports = {
   linearSearch,
   globalLinearSearch
-}
+};
