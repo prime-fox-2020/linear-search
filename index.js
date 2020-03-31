9342089556eb609b9182a85f3bf58b913e817b81
fadhlanfm <fafadhlanriz@gmail.com>
2020-03-31 22:19:50 +0700

    Release 0: OK

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(let i = 0; i < values.length; i++){
+    if(target === values[i]){
+      return i
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
