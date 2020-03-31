34d9441594d21732d813bc8c68bb893702c5e250
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-03-31 16:52:28 +0700

    finished file index.js

--- index.js
+++ index.js
@@ -3,10 +3,18 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  let n = values.length;
+  for (let i = 0 ; i < n; i ++) {
+    if (values[i] == target) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+
+
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -16,7 +24,15 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let res = [];
+
+  let n = values.length;
+  for (let i = 0; i < n; i++) {
+    if (values[i]=== target) {
+      res.push(i);
+    }
+  }
+  return res;
 }
 
 let banana_arr = "banana".split("");
